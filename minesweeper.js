41c3d3eedc6087bda9585e32f7deb28b585cfb92
Ellora Virtue <ellora.virtue@hotmail.com>
2020-03-02 15:44:09 +1300

    Update code to declare when player wins

--- minesweeper.js
+++ minesweeper.js
@@ -6,47 +6,47 @@ var board = {
     row: 0,
     col: 0,
     isMine: true,
-    hidden: true
+    hidden: true,
   }, {
     row: 0,
     col: 1,
     isMine: false,
-    hidden: true
+    hidden: true,
   }, {
     row: 0,
     col: 2,
     isMine: false,
-    hidden: true
+    hidden: true,
   }, {
     row: 1,
     col: 0,
     isMine: false,
-    hidden: true
+    hidden: true,
   }, {
     row: 1,
     col: 1,
     isMine: true,
-    hidden: true
+    hidden: true,
   }, {
     row: 1,
     col: 2,
     isMine: true,
-    hidden: true
+    hidden: true,
   }, {
     row: 2,
     col: 0,
     isMine: false,
-    hidden: true
+    hidden: true,
   }, {
     row: 2,
     col: 1,
     isMine: false,
-    hidden: true
+    hidden: true,
   }, {
     row: 2,
     col: 2,
     isMine: false,
-    hidden: true
+    hidden: true,
   }]
 }
 
@@ -60,16 +60,31 @@ function startGame () {
   lib.initBoard()
 }
 
+document.addEventListener("click", checkForWin)
+document.addEventListener("contextmenu", checkForWin)
+
+
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
 
+  for (var i = 0; i < board.cells.length; i++) {
+    if (board.cells[i].isMine == true && !board.cells[i].isMarked) {
+      return
+    } else if (board.cells[i].isMine == false && board.cells[i].hidden == true) {
+      return
+    }
+  }
+  
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+  lib.displayMessage('You win!')
+
 }
+    
+
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
@@ -92,5 +107,3 @@ function countSurroundingMines(cell) {
 
   return count
 }
-
-console.log(countSurroundingMines(board.cells[0]))
